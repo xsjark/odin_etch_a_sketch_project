dc7bcdf22bf732b593b326436f8a30a98911bad5
Ridha Watanabe <xsjark@protonmail.com>
2022-08-20 17:58:11 -0500

    can set squares

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
 
-const n = 16
+
 const container = document.querySelector("#container");
 
-for(let i = 1; i <= n**2; i++) {
+const renderGrid = (n=16) => {
+  for (let i = 1; i <= n ** 2; i++) {
+    document.documentElement.style.setProperty("--rowscols", n);
     const gridItem = document.createElement("div");
     gridItem.classList.add("grid-item");
     gridItem.setAttribute("id", i);
     gridItem.textContent = i;
-    container.appendChild(gridItem)
-}
+    container.appendChild(gridItem);
+  }
+};
+const setSquares = () => {
+  let squares = parseInt(prompt("How many squares?"));
+  if (typeof squares === "number") {
+    n = squares;
+    renderGrid(squares);
+  } else {
+    alert("Enter a number");
+  }
+};
+
+renderGrid()
